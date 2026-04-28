e7ea63f7c879b2d30c400ecbd18be7f76ca64435
HevenDev <hevendevofficial@gmail.com>
2026-04-28 20:02:03 +0530

    Loops started in java

---
+++ Loops.java
@@ -0,0 +1,13 @@
+public class Loops {
+    public static void main(String args[]) {
+         int i = 1;
+
+         while (i<=4) {
+            System.out.println("Hii, " + i);
+            i++;
+         }
+
+         System.out.println("Bye, " + i);
+
+    }
+}
